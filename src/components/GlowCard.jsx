94dd8cbaaa9cfe0e52113793fb10caff399faa0a
Alexandra <cimentarowa@gmail.com>
2025-04-30 23:15:15 +0300

    feat: Experience section

---
+++ src/components/GlowCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GlowCard = ( {card, children} ) => {
+  return (
+    <div className='card card-border timeline-card rounded-xl p-10'>
+        <p className='glow'></p>
+    </div>
+  )
+}
+
+export default GlowCard
