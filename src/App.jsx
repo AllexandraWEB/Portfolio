58400ff34e4e715c8857bc21caae8999813a0439
Alexandra <cimentarowa@gmail.com>
2025-06-25 15:06:30 +0300

    add: Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Hero from './sections/Hero'
@@ -7,27 +7,41 @@ import NavBar from './components/NavBar'
 import Experience from './sections/Experience'
 import Footer from './sections/Footer'
 import LightFooter from './sections/LightFooter'
+import Preloader from './components/Preloader' 
 
 const App = () => {
+  const [isLoaded, setIsLoaded] = useState(false)
+
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Hero />
-            <ShowcaseSection />
-            <Footer />
-          </>
-        } />
-        <Route path="/experience" element={
-          <>
-          <Experience />
-          <LightFooter />
-          </>
-          } />
-      </Routes>
-    </Router>
+    <>
+      {!isLoaded && <Preloader onFinish={() => setIsLoaded(true)} />}
+      {isLoaded && (
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <ShowcaseSection />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/experience"
+              element={
+                <>
+                  <Experience />
+                  <LightFooter />
+                </>
+              }
+            />
+          </Routes>
+        </Router>
+      )}
+    </>
   )
 }
 
---
+++ src/components/Preloader.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+import { CustomEase } from "gsap/CustomEase";
+
+const Preloader = ({ onFinish }) => {
+  const [hideLoader, setHideLoader] = useState(false);
+  const [showTitle, setShowTitle] = useState(false);
+
+  const containerRef = useRef(null);
+  const counterRef = useRef(null);
+
+  const progressChunks = [
+    [0, 0],
+    [2, 7],
+    [6, 5],
+    [9, 8],
+    [9, 9],
+  ];
+
+  gsap.registerPlugin(CustomEase);
+  CustomEase.create("hop", "0.9, 0, 0.1, 1");
+
+  useEffect(() => {
+    const counts = counterRef.current.querySelectorAll(".count");
+    const tl = gsap.timeline({ onComplete: animateOut });
+
+    const enterDuration = 0.5;
+    const exitDuration = 0.5;
+    const stagger = 0.075;
+    const waitTimeAfterEnter = 0.1;
+
+    counts.forEach((count, index) => {
+      const digits = count.querySelectorAll(".digit h1");
+
+      const totalStaggerTime = stagger * (digits.length - 1);
+
+      const startTime =
+        index * (enterDuration + exitDuration + waitTimeAfterEnter + 0.2);
+
+      // Enter
+      tl.to(
+        digits,
+        {
+          y: "0%",
+          duration: enterDuration,
+          stagger: stagger,
+          ease: "power2.out",
+        },
+        startTime
+      );
+
+      // Exit
+      tl.to(
+        digits,
+        {
+          y: "-100%",
+          duration: exitDuration,
+          stagger: stagger,
+          ease: "power2.in",
+        },
+        startTime + enterDuration + waitTimeAfterEnter
+      );
+    });
+
+    function animateOut() {
+      const tl2 = gsap.timeline();
+
+      tl2.fromTo(
+        containerRef.current,
+        { opacity: 1 },
+        {
+          opacity: 0,
+          duration: 0.6,
+          delay: 0.5,
+          ease: "hop",
+          onComplete: () => {
+            setHideLoader(true);
+            onFinish();
+          },
+        }
+      );
+    }
+  }, [onFinish]);
+
+  if (hideLoader) return null;
+
+  return (
+    <div
+      ref={containerRef}
+      className="fixed inset-0 bg-black flex items-center justify-center z-50"
+    >
+      <div
+        className="counter absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20"
+        ref={counterRef}
+      >
+        {progressChunks.map((pair, i) => (
+          <div
+            key={i}
+            className="count flex absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20"
+          >
+            {pair.map((digit, j) => (
+              <div
+                key={j}
+                className="digit overflow-hidden h-[70px] md:h-[170px] w-[50px] md:w-[90px] flex items-center justify-center"
+              >
+                <h1
+                  className="text-white text-[70px] md:text-[170px] font-bold font-main"
+                  style={{ transform: "translateY(100%)" }}
+                >
+                  {digit}
+                </h1>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Preloader;
