834a80ac0cb5c86d812ee71a16090c0e77867e6f
Alexandra <cimentarowa@gmail.com>
2025-07-13 21:50:28 +0300

    bug: Removed OrbitControls for mobile devices

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -9,21 +9,28 @@ const HeroExperience = () => {
   const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
+  // Set angles based on device
+  const maxPolarAngle = isMobile ? Math.PI / 4 : Math.PI / 5;
+  const minPolarAngle = isMobile ? Math.PI / 3 : Math.PI / 3.7;
+
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-      <OrbitControls
-        enablePan={false}
-        enableZoom={false}
-        maxPolarAngle={Math.PI / 5}
-        minPolarAngle={Math.PI / 3.7}
-      />
+      {!isMobile && ( // OrbitControls is not allowed on mobile, this way any touch events won't be interrupted by OrbitControls and will allow normal scrollwing
+        <OrbitControls
+          enablePan={false}
+          enableZoom={false}
+          enableRotate={true}
+          maxPolarAngle={maxPolarAngle}
+          minPolarAngle={minPolarAngle}
+        />
+      )}
 
       <HeroLights />
 
       <group
-        scale={isMobile ? 2.1 : 3.4} // 4.8 or 2.8
-        position={[0, -1, 0]} //3.8
-        rotation={[0, -Math.PI / 4, 0]} // 6.7, -Math.PI / 4, 0
+        scale={isMobile ? 2.1 : 3.4}
+        position={[0, -1, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
       >
         <RotatingLotus />
       </group>
